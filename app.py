870b277e2d3f27efa463331f679aef1e4235888c
zzazzz <ziyad.azzufari@gmail.com>
2026-04-07 10:23:48 +0700

    update

--- app.py
+++ app.py
@@ -492,4 +492,5 @@ def detect_object():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
